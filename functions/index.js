11791c21c31c960f0beec060bfb533e967c61276
Nick Meinhold <nick@enspyr.co>
2024-06-12 17:34:16 +1000

    save doc on new user

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,25 @@
 /**
+ * Import function triggers from their respective submodules:
  *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {AccessToken} from "livekit-server-sdk";
-const functions = require("firebase-functions/functions");
+const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access Firestore.
-const firebaseAdmin = require("firebase-admin");
-firebaseAdmin.initializeApp();
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-exports.saveToken = functions.auth.user().onCreate(async (user) => {
-  // if this room doesn't exist, it'll be automatically created when the first
-  // client joins
-  const roomName = "quickstart-room";
-  // identifier to be used for participant.
-  // it's available as LocalParticipant.identity with livekit-client SDK
-  const participantName = user.displayName;
-
-  const at = new AccessToken(
-      process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_API_SECRET, {
-        identity: participantName,
-        // token to expire after 10 minutes
-        ttl: "10m",
-      });
-  at.addGrant({roomJoin: true, room: roomName});
-
-  const token = await at.toJwt();
-
-  // Push the token into Firestore using the Firebase Admin SDK.
-  const writeResult = await firebaseAdmin.getFirestore()
-      .collection("tokens")
-      .add({original: user.uid, token: token});
-
-  functions.logger.log("Token added to ", writeResult.path);
+exports.saveDoc = functions.auth.user().onCreate(async (user) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await admin
+    .firestore()
+    .collection("users")
+    .doc(user.uid).set({ original: user.email });
+  // Send back a message that we've successfully written the message
+  functions.logger.info("Doc with ID: ${writeResult.id} added.");
 });
